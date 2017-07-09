fdb1e97dad3e10033829d6c3488dfd8462dfcb70
riquet28 <eric.merlin3@gmail.com>
2017-07-09 02:07:28 +0200

    Add Youtube Api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+const API_KEY = 'AIzaSyDKPo7ouVbLwqex7rU2oBPqN4qBqoEtlAE';
+
 const App = () => {
-  return <div>Hi!</div>
+  return <div>Hi!</div>;
 }
 
 ReactDom.render(<App />, document.querySelector('.container'))
