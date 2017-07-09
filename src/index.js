1b188329f9b322c0e484662d056bbb7a5c296216
riquet28 <eric.merlin3@gmail.com>
2017-07-09 18:19:58 +0200

    Start with react ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDKPo7ouVbLwqex7rU2oBPqN4qBqoEtlAE';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDom.render(<App />, document.querySelector('.container'))
